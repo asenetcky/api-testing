6ac4b335f84d42a1661624249da1b8fe117cf0eb
asenetcky <me@asenetcky.dev>
2025-06-19 02:57:12 +0000

    types because why not

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use polars::prelude::*;
-
+use std::fs::File;:walking
 
 fn main() {
     playground()
 }
 
 fn playground() {
-    let mut file = std::fs::File::open("storage-format.parquet").unwrap();
+    let mut file: File = std::fs::File::open("storage-format.parquet").unwrap();
 
-    let df = ParquetReader::new(&mut file).finish().unwrap();
+    let df: DataFrame = ParquetReader::new(&mut file).finish().unwrap();
     println!("{}", df);
     println!("{:?}", df.drop_many([
         "row_id",
